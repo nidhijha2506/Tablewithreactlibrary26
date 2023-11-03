4f86f1744f8246a5795f07c9eaabe28d50e16b67
nidhijha2506 <nidhijhagudia@gmail.com>
2023-11-03 13:35:49 +0530

    form submission msg added

--- src/Editmodal.jsx
+++ src/Editmodal.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import { useForm } from "react-hook-form";
+import { Button } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
 import "./Style.css";
 
 // const getLocalDetails =()=>
@@ -16,143 +18,207 @@ import "./Style.css";
 //     }
 // }
 
-const EditButtonModel =( {closeModel,tabledata,show} )=>
-    {
-        const [editdetails,seteditdetails]= useState();
-        const [Tabledata,setTabledata]= useState();
+const EditButtonModel = ({ closeModel, tabledata, showw, hideModal }) => {
+ // const [editdetails, seteditdetails] = useState();
+  //const [Tabledata, setTabledata] = useState(tabledata);
+  //const [show, setShow] = useState(false);
 
-        const {
-            register, formState: { errors }, handleSubmit, } = useForm();
-            // const [tableData,setTableData]=useState();
-          // const [inputdetails,setinputdetails]= useState({});
-        //   const [inputdata, setinputdata] = useState([]);
-        //   const [show, setShow] = useState(false);
-        
-        //   const handleClose = () => setShow(false);
-        //   const handleShow = () => setShow(true);
-        // const defaultName=tabledata.Name;
+  //const handleClose = () => setShow(false);
+  //const handleShow = () => setShow(true);
+  const {
+    register, formState: { errors }, handleSubmit, } = useForm();
+  // const [tableData,setTableData]=useState();
+  // const [inputdetails,setinputdetails]= useState({});
+  //   const [inputdata, setinputdata] = useState([]);
+  //   const [show, setShow] = useState(false);
 
-   
+  //   const handleClose = () => setShow(false);
+  //   const handleShow = () => setShow(true);
+  // const defaultName=tabledata.Name;
 
-    const onSubmit=(data)=>
-    {
-       
 
-        console.log("tabledata",data);
-       
-        //event.preventDefault();
+  const [successMsg,setSucessMsg]=useState();
+  const onSubmit = (data) => {
 
-          tabledata.firstName = data.firstName;
-          tabledata.lastName=data.lastName;
-          tabledata.branch=data.branch;
-          tabledata.age=data.age;
-          tabledata.section=data.section;
-          tabledata.rollno=data.rollno;
-          tabledata.email=data.email;
-          tabledata.code=data.code;
-          tabledata.phone=data.phone;
-          
-         
-         
-    }
 
-    // useEffect(()=>
-    // {
-    //     //console.log('Before storing in local storage:', inputdata);
-    //     localStorage.setItem('details',JSON.stringify(Tabledata))
-    //     //console.log('After storing in local storage:', inputdata);
-    // }, [Tabledata]);
+    console.log("tabledata", data);
     
-        return(
-            <>
-            {/* onClick={closeModel} */}
-             
-        <form className=" text-center edit_form"  onSubmit={handleSubmit(onSubmit)}>
-        <label className="fw-bold mt-3 name-label">Full Name&nbsp;&nbsp;
-          <input  defaultValue={tabledata.firstName}  {...register("firstName", {
-            required: "First name is required",
-            maxLength: 20, pattern: {
-              value: /^[A-Za-z]+$/i,
-              message: "only starts with letter"
-            }
-          })}
-            aria-invalid={errors.firstName ? "true" : "false"} />&nbsp;&nbsp;
-          <input  defaultValue={tabledata.lastName}  {...register("lastName", {
-            required: 'Last name is required',
-            maxLength: 20, pattern: /^[A-Za-z]+$/i
-          })}
-            aria-invalid={errors.lastName ? "true" : "false"}
-          />
-          {errors.firstName && <h6 className="text-danger name-alert" role="alert">{errors.firstName.message}</h6>}
-          {errors.lastName && <h6 className="text-danger lastname-alert" role="alert">{errors.lastName.message}</h6>}
-        </label><br /><br />
-
-        <label className="fw-bold label-age">Age
-          <input  defaultValue={tabledata.age} className="input-age" type="text" {...register("age",
-            { required: 'Age is required', min: 18, max: 99 })}
-            aria-invalid={errors.age ? "true" : "false"} />
-          {errors.age && <h6 className="text-danger age-alert" role="alert">{errors.age.message}</h6>}
-        </label>
-
-        <label className="fw-bold rollno-label">Roll no
-          <input  defaultValue={tabledata.rollno} className="rollno-input" type="number" {...register("rollno",
-            { required: 'Roll no is required', min: 1, max: 100 })}
-            aria-invalid={errors.rollno ? "true" : "false"} />
-          {errors.rollno && <h6 className="text-danger rollno-alert" role="alert">{errors.rollno.message}</h6>}
-        </label><br /><br />
-
-
-        <label className="fw-bold branch-label">Branch
-          <select  defaultValue={tabledata.branch} className="branch-select" {...register("branch", { required: 'Branch is required' })}
-            aria-invalid={errors.branch ? "true" : "false"}>
-            <option value="">Branch</option>
-            <option value="ME">ME</option>
-            <option value="EE">EE</option>
-            <option value="EC">EC</option>
-            <option value="IT">IT</option>
-            <option value="CS">CS</option>
-            <option value="CE">CE</option>
-          </select>
-          {errors.branch && <h6 className="text-danger branch-alert" role="alert">{errors.branch.message}</h6>}
-        </label>
-
-        <label className="fw-bold section-label">Section
-          <select  defaultValue={tabledata.section} className="section-select" {...register("section", { required: 'Section is required' })}
-            aria-invalid={errors.branch ? "true" : "false"}>
-            <option value="">Section</option>
-            <option value="A">A</option>
-            <option value="B">B</option>
-            <option value="C">C</option>
-            <option value="D">D</option>
-            <option value="E">E</option>
-            <option value="F">F</option>
-          </select>
-          {errors.section && <h6 className="text-danger section-alert" role="alert">{errors.section.message}</h6>}
-        </label><br /><br />
-
-
-        <label className="fw-bold email-label">Email
-          <input  defaultValue={tabledata.email} className="email-input" {...register("email", { required: 'Email is required', pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i })}
-            aria-invalid={errors.email ? "true" : "false"} />
-          {errors.email && <h6 className="text-danger email-alert" role="alert">{errors.email.message}</h6>}
-        </label><br /><br />
-
-        <label className="fw-bold phone-label">Phone No.
-          <input  defaultValue={tabledata.code} className="code-input" type="text" {...register("code",
-            { required: "Code is required", maxLength: 2 })}
-            aria-invalid={errors.code ? "true" : "false"} />&nbsp;
-          <input  defaultValue={tabledata.phone} className="phone-input" type="text" {...register("phone",
-            { required: 'Phone is required', maxLength: 10 })}
-            aria-invalid={errors.phone ? "true" : "false"} />
-          {errors.code && <h6 className="text-danger code-alert" role="alert">{errors.code.message}</h6>}
-          {errors.phone && <h6 className="text-danger phone-alert" role="alert">{errors.phone.message}</h6>}
-        </label><br /><br />
-        <button>Update</button>
-      </form><br /><br />
-             
-            </>
-        );
+    //event.preventDefault();
+    function delay(wait) {
+      return new Promise((resolve) => {
+        setTimeout(resolve, wait);
+      });
     }
+    async function synchronousSetTimeout() {
 
-    export default EditButtonModel;
+      setTimeout(() => {
+        setSucessMsg("Your form updated successfully")
+      }, 2000);
+      
+    await delay(4000);
+
+    tabledata.firstName = data.firstName;
+    tabledata.lastName = data.lastName;
+    tabledata.branch = data.branch;
+    tabledata.age = data.age;
+    tabledata.section = data.section;
+    tabledata.rollno = data.rollno;
+    tabledata.email = data.email;
+    tabledata.code = data.code;
+    tabledata.phone = data.phone;
+
+    //localStorage.setItem('tableData', JSON.stringify(tabledata));
+
+    console.log('End');
+    hideModal();
     
+    
+}
+
+synchronousSetTimeout();
+  }
+
+  // useEffect(()=>
+  // {
+  //     //console.log('Before storing in local storage:', inputdata);
+  //     localStorage.setItem('details',JSON.stringify(Tabledata))
+  //     //console.log('After storing in local storage:', inputdata);
+  // }, [Tabledata]);
+
+  // useEffect(() => {
+  //   // 3. Use localStorage to store data when it changes
+  //   localStorage.setItem('tableData', JSON.stringify(Tabledata));
+  // }, [Tabledata]);
+
+  // useEffect(() => {
+  //   // Load data from local storage when the component mounts
+  //   const storedData = localStorage.getItem('tableData');
+  //   if (storedData) {
+  //     setTabledata(JSON.parse(storedData));
+  //   }
+  // }, []);
+  
+
+  return (
+    <>
+      {/* onClick={closeModel} */}
+
+      <Modal show={showw} onHide={()=>hideModal()}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered>
+        <Modal.Header closeButton className="bg-dark">
+          <Modal.Title><h3 className=" text-white fs-1 py-2  stu_details">Edit Details</h3></Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form className=" text-center edit_form" onSubmit={handleSubmit(onSubmit)}>
+            <label className="fw-bold mt-3 name-label">Full Name&nbsp;&nbsp;
+              <input defaultValue={tabledata.firstName}  {...register("firstName", {
+                required: "First name is required",
+                maxLength: 20, pattern: {
+                  value: /^[A-Za-z]+$/i,
+                  message: "only starts with letter"
+                }
+              })}
+                aria-invalid={errors.firstName ? "true" : "false"} />&nbsp;&nbsp;
+              <input defaultValue={tabledata.lastName}  {...register("lastName", {
+                required: 'Last name is required',
+                maxLength: 20, pattern: {
+                  value: /^[A-Za-z]+$/i,
+                  message: "only starts with letter"
+                }
+              })}
+                aria-invalid={errors.lastName ? "true" : "false"}
+              />
+              {errors.firstName && <h6 className="text-danger name-alert" role="alert">{errors.firstName.message}</h6>}
+              {errors.lastName && <h6 className="text-danger lastname-alert" role="alert">{errors.lastName.message}</h6>}
+            </label><br /><br />
+
+            <label className="fw-bold label-age">Age
+              <input defaultValue={tabledata.age} className="input-age" type="text" {...register("age",
+                { required: 'Age is required', min:{value: 18,
+                  message: "Min age should be 18" }, max: {value:30, message:'Max age should be 30'} })}
+                aria-invalid={errors.age ? "true" : "false"} />
+              {errors.age && <h6 className="text-danger age-alert" role="alert">{errors.age.message}</h6>}
+            </label>
+
+            <label className="fw-bold rollno-label">Roll no
+              <input defaultValue={tabledata.rollno} className="rollno-input" type="number" {...register("rollno",
+                { required: 'Roll no is required', min:{value: 1,
+                  message: "Least roll no is 1" }, max: {value:100, message:'Last roll no is 100'} })}
+                aria-invalid={errors.rollno ? "true" : "false"} />
+              {errors.rollno && <h6 className="text-danger rollno-alert" role="alert">{errors.rollno.message}</h6>}
+            </label><br /><br />
+
+
+            <label className="fw-bold branch-label">Branch
+              <select defaultValue={tabledata.branch} className="branch-select" {...register("branch", { required: 'Branch is required' })}
+                aria-invalid={errors.branch ? "true" : "false"}>
+                <option value="">Branch</option>
+                <option value="ME">ME</option>
+                <option value="EE">EE</option>
+                <option value="EC">EC</option>
+                <option value="IT">IT</option>
+                <option value="CS">CS</option>
+                <option value="CE">CE</option>
+              </select>
+              {errors.branch && <h6 className="text-danger branch-alert" role="alert">{errors.branch.message}</h6>}
+            </label>
+
+            <label className="fw-bold section-label">Section
+              <select defaultValue={tabledata.section} className="section-select" {...register("section", { required: 'Section is required' })}
+                aria-invalid={errors.branch ? "true" : "false"}>
+                <option value="">Section</option>
+                <option value="A">A</option>
+                <option value="B">B</option>
+                <option value="C">C</option>
+                <option value="D">D</option>
+                <option value="E">E</option>
+                <option value="F">F</option>
+              </select>
+              {errors.section && <h6 className="text-danger section-alert" role="alert">{errors.section.message}</h6>}
+            </label><br /><br />
+
+
+            <label className="fw-bold email-label">Email
+              <input defaultValue={tabledata.email} className="email-input"
+               {...register("email", { required: 'Email is required',
+               pattern: {value:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i ,message:'Email is not valid'}})}
+                aria-invalid={errors.email ? "true" : "false"} />
+              {errors.email && <h6 className="text-danger email-alert" role="alert">{errors.email.message}</h6>}
+            </label><br /><br />
+
+            <label className="fw-bold phone-label">Phone No.
+              <input defaultValue={tabledata.code} className="code-input" type="text" {...register("code",
+                { required: "Code is required", pattern:{ value: /^91+$/i, message:' 91'}, 
+                maxLength: {value:2,message:'Max length is 2'},
+                 minLength: {value:2,message:'Min length is 2'} })}
+                aria-invalid={errors.code ? "true" : "false"} />&nbsp;
+              <input defaultValue={tabledata.phone} className="phone-input" type="text" {...register("phone",
+                { required: 'Phone is required',maxLength: {value:10, message:'Max length should be 10'},
+                minLength:{value:10, message:'Min length should be 10'} })}
+                aria-invalid={errors.phone ? "true" : "false"} />
+              {errors.code && <h6 className="text-danger code-alert" role="alert">{errors.code.message}</h6>}
+              {errors.phone && <h6 className="text-danger phone-alert" role="alert">{errors.phone.message}</h6>}
+            </label><br /><br /><br />
+            <button className="bg-success text-white  fs-5 w-25 rounded update_btn">Update</button>
+          </form><br /><br />
+          <h3 className="text-success bg-muted h-25 fs-4 text-center">{successMsg}</h3>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => hideModal()}>
+            Close
+          </Button>
+          {/* <Button type="submit" variant="primary" onClick={() => hideModal()}>
+            Update
+          </Button> */}
+        </Modal.Footer>
+      </Modal>
+
+
+
+    </>
+  );
+}
+
+export default EditButtonModel;
--- src/Formwithlibrary.jsx
+++ src/Formwithlibrary.jsx
@@ -2,36 +2,37 @@ import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import 'bootstrap/dist/css/bootstrap.css';
 import { Button } from "react-bootstrap";
-import { Modal } from "react-bootstrap";
+//import { Modal } from "react-bootstrap";
 import "./Style.css";
 import EditButtonModel from "./Editmodal";
 
-// const getLocalDetails =()=>
-// {
-//     let stu_details = localStorage.getItem('details');
-//     //console.log(stu_details);
-//     if(stu_details)
-//     {
-//         return JSON.parse(localStorage.getItem('details'))
-//     }
-//     else{
-//         return [];
-//     }
-// }
-
-
-
+const getLocalDetails =()=>
+{
+    let stu_details = localStorage.getItem('details');
+    //console.log(stu_details);
+    if(stu_details)
+    {
+        return JSON.parse(localStorage.getItem('details'))
+    }
+    else{
+        return [];
+    }
+}
 export default function App(props) {
   const {
     register, reset, formState: { errors }, handleSubmit, } = useForm();
     const [tableData,setTableData]=useState();
   // const [inputdetails,setinputdetails]= useState({});
-  const [inputdata, setinputdata] = useState([]);
-  const [show, setShow] = useState(false);
+  const [inputdata, setinputdata] = useState(getLocalDetails());
+  //const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  // const handleClose = () => setShow(false);
+  //const handleShow = () => setShow(true);
+  const [modalShow, setModalShow] = useState(false);
 
+  // useEffect(()=>{
+
+  // },[modalShow])
 
   // const handleChange =(event)=>
   // {
@@ -56,9 +57,12 @@ export default function App(props) {
   function editTable(index)
   {
     
-    handleShow(true);
+   //modalShow(true);
     setTableData(inputdata.find((fn,ind)=> ind === index));
+    console.log('hello');
+    setModalShow(true);
   }
+  console.log('hello',modalShow);
 
   function handledelete(fid) {
     // alert(fid);
@@ -75,13 +79,19 @@ export default function App(props) {
     setinputdata([]);
   }
 
-  // useEffect(()=>
-  // {
-  //     //console.log('Before storing in local storage:', inputdata);
-  //     localStorage.setItem('details',JSON.stringify(inputdata))
-  //     //console.log('After storing in local storage:', inputdata);
-  // }, [inputdata]);
+ 
 
+  const hideModal = ()=>{
+    console.log("Working");
+    setModalShow(false)
+  }
+
+  useEffect(()=>
+  {
+      //console.log('Before storing in local storage:', inputdata);
+      localStorage.setItem('details',JSON.stringify(inputdata))
+      //console.log('After storing in local storage:', inputdata);
+  }, [inputdata]);
 
 
 
@@ -101,7 +111,10 @@ export default function App(props) {
             aria-invalid={errors.firstName ? "true" : "false"} />&nbsp;&nbsp;
           <input  {...register("lastName", {
             required: 'Last name is required',
-            maxLength: 20, pattern: /^[A-Za-z]+$/i
+            maxLength: 20, pattern: {
+              value: /^[A-Za-z]+$/i,
+              message: "only starts with letter"
+            }
           })}
             aria-invalid={errors.lastName ? "true" : "false"}
           />
@@ -111,14 +124,16 @@ export default function App(props) {
 
         <label className="fw-bold label-age">Age
           <input className="input-age" type="text" {...register("age",
-            { required: 'Age is required', min: 18, max: 99 })}
+            { required: 'Age is required', min:{value: 18,
+              message: "Min age should be 18" }, max: {value:30, message:'Max age should be 30'}})}
             aria-invalid={errors.age ? "true" : "false"} />
           {errors.age && <h6 className="text-danger age-alert" role="alert">{errors.age.message}</h6>}
         </label>
 
         <label className="fw-bold rollno-label">Roll no
           <input className="rollno-input" type="number" {...register("rollno",
-            { required: 'Roll no is required', min: 1, max: 100 })}
+            { required: 'Roll no is required', min:{value: 1,
+              message: "Least roll no is 1" }, max: {value:100, message:'Last roll no is 100'} })}
             aria-invalid={errors.rollno ? "true" : "false"} />
           {errors.rollno && <h6 className="text-danger rollno-alert" role="alert">{errors.rollno.message}</h6>}
         </label><br /><br />
@@ -154,24 +169,26 @@ export default function App(props) {
 
 
         <label className="fw-bold email-label">Email
-          <input className="email-input" {...register("email", { required: 'Email is required', pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i })}
+          <input className="email-input" {...register("email", { required: 'Email is required', 
+          pattern: {value:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i ,message:'Email is not valid'}})}
             aria-invalid={errors.email ? "true" : "false"} />
           {errors.email && <h6 className="text-danger email-alert" role="alert">{errors.email.message}</h6>}
         </label><br /><br />
 
         <label className="fw-bold phone-label">Phone No.
           <input className="code-input" type="text" {...register("code",
-            { required: "Code is required", maxLength: 2 })}
+            { required: "Code is required",pattern:{ value: /^91+$/i, message:' 91'}, maxLength: {value:2,message:'Max length is 2'}, minLength: {value:2,message:'Min length is 2'} })}
             aria-invalid={errors.code ? "true" : "false"} />&nbsp;
           <input className="phone-input" type="text" {...register("phone",
-            { required: 'Phone is required', maxLength: 10 })}
+            { required: 'Phone is required', maxLength: {value:10, message:'Max length should be 10'},
+             minLength:{value:10, message:'Min length should be 10'} })}
             aria-invalid={errors.phone? "true" : "false"} />
           {errors.code && <h6 className="text-danger code-alert" role="alert">{errors.code.message}</h6>}
           {errors.phone && <h6 className="text-danger phone-alert" role="alert">{errors.phone.message}</h6>}
         </label><br /><br />
 
-
-        <input type="submit" className=" bg-primary w-25 h5 fw-bold h-25 submit_btn" /><br /><br />
+        <Button type="submit" className=" bg-primary fs-5 w-25 h5 fw-bold h-25 submit_btn" >Submit</Button><br /><br />
+        {/* <input type="submit" className=" bg-primary w-25 h5 fw-bold h-25 submit_btn" /><br /><br /> */}
       </form><br /><br />
 
       <table cellPadding={10} cellSpacing={0} border={1} className="bg-secondary text-center">
@@ -185,8 +202,8 @@ export default function App(props) {
             <th>Roll No.</th>
             <th className="email_th">Email Id</th>
             <th className="phone_th">Phone No.</th>
-            <th className="delete_th">Edit</th>
-            <th>Delete</th>
+            <th className="edit_th">Edit</th>
+            <th className="delete_th">Delete</th>
           </tr>
         </thead>
         <tbody className="bg-white">
@@ -205,10 +222,13 @@ export default function App(props) {
                   <td>{data.email}</td>
                   <td>{data.code} {data.phone}</td>
                   <td>
-                    <Button variant="success" onClick={()=>editTable(index)}>
-                    Edit
+                    <Button variant="success" onClick={()=>editTable(index)} >
+                    Edit 
                    </Button>
-                    <Modal show={show} onHide={handleClose} {...props} 
+                   { modalShow  && 
+                   <EditButtonModel  showw={modalShow} hideModal={hideModal} tabledata={tableData} ></EditButtonModel>
+                   }
+                    {/* <Modal show={show} onHide={handleClose} {...props} 
                                  size="lg"
                                   aria-labelledby="contained-modal-title-vcenter"
                                    centered>
@@ -226,14 +246,19 @@ export default function App(props) {
                           Update
                         </Button>
                       </Modal.Footer>
-                    </Modal>
+                    </Modal> */}
                   </td>
                   <td>
-                    <button type="button" onClick={() => {
+                    <Button type="button" onClick={() => {
                       handledelete(index)
 
 
-                    }} className="bg-danger">Delete</button>
+                    }} className="bg-danger">Delete</Button>
+                    {/* <button type="button" onClick={() => {
+                      handledelete(index)
+
+
+                    }} className="bg-danger">Delete</button> */}
                   </td>
                 </tr>)
 
@@ -241,7 +266,8 @@ export default function App(props) {
           }
         </tbody>
       </table><br />
-      <button type="button" onClick={deletedetails} className="bg-danger reset_button">Reset All Details</button><br /><br />
+      <Button type="button" onClick={deletedetails} className="bg-danger reset_button">Reset All Details</Button><br /><br />
+      {/* <button type="button" onClick={deletedetails} className="bg-danger reset_button">Reset All Details</button><br /><br /> */}
     </>
   )
 }
